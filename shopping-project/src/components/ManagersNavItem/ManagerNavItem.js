02135123d78a9d4a06afe11cfeb05daad3866054
rasoul <rasoulkordestani12@gmail.com>
2021-07-02 12:44:26 +0430

    5_2_12 handle navigation with useHistory (and router in app.js file)

--- shopping-project/src/components/ManagersNavItem/ManagerNavItem.js
+++ shopping-project/src/components/ManagersNavItem/ManagerNavItem.js
@@ -1,26 +1,35 @@
 import React, { Component } from "react";
-
-export class ManagerNavItem extends Component {
-  render() {
-    return (
-      <div style={{ display: "flex" }}>
-        <ul>
-          <li>
-            <a href="#">کالاها</a>
-          </li>
-          <li>
-            <a href="#">موجودی و قیمت ها</a>
-          </li>
-          <li>
-            <a href="#">سفارش ها</a>
-          </li>
-        </ul>
-        <div>
-          <a href="#">بازگشت به سایت</a>
-        </div>
-      </div>
-    );
+import { useHistory } from 'react-router-dom';
+import Button from '@material-ui/core/Button';
+export function ManagerNavItem() {
+  let history = useHistory();
+  function productPage() {
+    history.push('/ProductTable')
   }
+  function StockPricePage() {
+    history.push('/StockPriceTable')
+  }
+  function customPage() {
+    history.push('/CustomTable')
+  }
+  function firstPage() {
+    history.push('/')
+  }
+  return (
+    <div>
+      <div>
+        <Button color="purpule" type="button" onClick={productPage}> مدیریت کالاها</Button>
+        <Button type="button" onClick={StockPricePage}> مدیریت موجودی ها </Button>
+        <Button type="button" onClick={customPage}> مدیریت سفارش ها</Button>
+      <Button type="button" onClick={firstPage}>بازگشت به صفحه اصلی</Button>
+      </div>
+    </div>
+  );
+
 }
 
-export default ManagerNavItem;
+
+
+
+
+
--- shopping-project/src/pages/App.js
+++ shopping-project/src/pages/App.js
@@ -1,12 +1,42 @@
 import React, { Component } from "react";
 import { Header, Section } from "../layouts/terminal";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { CustomPage } from '../pages/Manager/CustomPage';
+import { ProductPage } from '../pages/Manager/ProductPage';
+import { StockPricePage } from '../pages/Manager/StockPricePage';
+import '../assets/style/style.css'
+
+
+
+
+
+
 export class App extends Component {
   render() {
     return (
-      <div>
-        <Header />
-        <Section />
-      </div>
+     
+
+        <BrowserRouter >
+
+
+          <Header />
+          <Section />
+          <Switch>
+            <Route exact path="/salam">
+              <h1>homePage</h1>
+            </Route>
+            <Route exact path="/CustomTable">
+              <CustomPage />
+            </Route>
+            <Route exact path="/ProductTable">
+              <ProductPage />
+            </Route>
+            <Route exact path="/StockPriceTable">
+              <StockPricePage />
+            </Route>
+          </Switch>
+        </BrowserRouter>
+    
     );
   }
 }
