dc4e3c46e174629e0c026e2ea00cade404da92d3
rasoul <rasoulkordestani12@gmail.com>
2021-07-12 08:41:02 +0430

    create MainHeader and ManagerHeader with their terminal

---
+++ shopping-project/src/layouts/Header/MainHeader.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import { LogoArea } from "../../components/terminal";
+import { ManagerNavItem } from "../../components/ManagersNavItem/ManagerNavItem";
+import Button from "@material-ui/core/Button";
+import Grid from "@material-ui/core/Grid";
+import "../../assets/styles/style.css"
+import { useHistory } from 'react-router-dom';
+
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+
+export function MainHeader() {
+  let history = useHistory();
+  function firstPage() {
+    history.push('/')
+  }
+
+  return (
+    <header >
+      <Grid  container color="white" alignItems="center" direction="row" justify="space-between">
+        <LogoArea />
+        
+        <div>
+          <Button type="button" onClick={firstPage}>مدیریت</Button>
+          <Button type="button" onClick={firstPage}> <ShoppingCartIcon />سبد خرید</Button>
+        </div>
+      </Grid>
+    </header>
+  );
+
+}
--- shopping-project/src/layouts/terminal.js
+++ shopping-project/src/layouts/terminal.js
@@ -1,2 +1,3 @@
-export { Header } from "./Header/Header";
+export { ManagerHeader } from "./Header/ManagerHeader";
+export { MainHeader } from './Header/MainHeader';
 export { Section } from './Section/Section'
