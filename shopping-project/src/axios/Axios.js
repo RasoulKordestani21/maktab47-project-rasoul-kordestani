6ad3a462f79ba672d7f6d964b73857a29dcdc173
rasoul <rasoulkordestani12@gmail.com>
2021-07-12 08:36:47 +0430

    develop axios (with more request's function)

--- shopping-project/src/axios/Axios.js
+++ shopping-project/src/axios/Axios.js
@@ -5,7 +5,45 @@ export const getUser = async (a=3,b=5) => {
   let response;
   try {
     const one = 1;
-    response = await axios.get(`http://localhost:3000/prodManTable?_start=${a}&_limit=${b}`);
+    response = await axios.get(`http://localhost:3000/milks?_start=${a}&_limit=${b}`);
+    // console.log(response);
+    
+  } catch (error) {
+    // console.error(error);
+  }
+  return response
+}
+
+export const getDairy = async (a=3,b=5) => {
+  let response;
+  try {
+    const one = 1;
+    response = await axios.get(`http://localhost:3000/dairy?_start=${a}&_limit=${b}`);
+    // console.log(response);
+    
+  } catch (error) {
+    // console.error(error);
+  }
+  return response
+}
+
+export const getHealthWashing = async (a=3,b=5) => {
+  let response;
+  try {
+    const one = 1;
+    response = await axios.get(`http://localhost:3000/health-Washing?_start=${a}&_limit=${b}`);
+    // console.log(response);
+    
+  } catch (error) {
+    // console.error(error);
+  }
+  return response
+}
+
+export const getProductDetail = async (category,id) => {
+  let response;
+  try {
+    response = await axios.get(`http://localhost:3000/${category}?id=${id}`);
     // console.log(response);
     
   } catch (error) {
