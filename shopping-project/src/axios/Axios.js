bc25ebc54c0c4159d63122f8235f25183fdfe42d
rasoul <rasoulkordestani12@gmail.com>
2021-07-14 00:41:54 +0430

    change a,b to start,limit and post data of user

--- shopping-project/src/axios/Axios.js
+++ shopping-project/src/axios/Axios.js
@@ -14,11 +14,11 @@ export const getUser = async (a=3,b=5) => {
   return response
 }
 
-export const getDairy = async (a=3,b=5) => {
+export const getDairy = async (start=3,limit=5) => {
   let response;
   try {
     const one = 1;
-    response = await axios.get(`http://localhost:3000/dairy?_start=${a}&_limit=${b}`);
+    response = await axios.get(`http://localhost:3000/dairy?_start=${start}&_limit=${limit}`);
     // console.log(response);
     
   } catch (error) {
@@ -50,6 +50,23 @@ export const getProductDetail = async (category,id) => {
     // console.error(error);
   }
   return response
+}
+
+export const postUserData = async (userData) => {
+  
+  await axios.post('http://localhost:3000/userSpecifics', {
+    firstName: userData.firstName,
+    lastName: userData.lastName,
+    address:userData.address,
+    phoneNumber:userData.phoneNumber,
+    dateOfSend:userData.dateOfSend
+  })
+  .then(function (response) {
+    console.log(response.data);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
 }
 
 
