c8685da8a093bd5914213874d4c82e18cea12325
rasoul <rasoulkordestani12@gmail.com>
2021-07-14 00:53:35 +0430

    create success and cancel sections and pages

---
+++ shopping-project/src/pages/Main/SuccessPay.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { MainHeader } from '../../layouts/terminal';
+import  SuccessPaySection  from '../../layouts/Section/MainSection/SuccessPaySection'
+export default class SuccessPay extends Component {
+    render() {
+        return (
+            <div>
+                <MainHeader />
+                <SuccessPaySection />
+            </div>
+        )
+    }
+}
