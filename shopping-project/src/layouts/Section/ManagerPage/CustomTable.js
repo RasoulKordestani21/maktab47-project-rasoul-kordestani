89694d35c2bff9264b7cb9e0e8854a1d3dee6ab1
rasoul <rasoulkordestani12@gmail.com>
2021-07-15 17:32:58 +0430

    apply filtering with redux reducers and action

--- shopping-project/src/layouts/Section/ManagerPage/CustomTable.js
+++ shopping-project/src/layouts/Section/ManagerPage/CustomTable.js
@@ -13,6 +13,8 @@ import Typography from '@material-ui/core/Typography';
 import Pagination from '@material-ui/lab/Pagination';
 import Box from '@material-ui/core/Box'
 
+import { connect } from 'react-redux';
+import { request } from "dom-helpers/cjs/animationFrame";
 
 // import { getUser } from '../axios/Axios'
 
@@ -41,64 +43,82 @@ const useStyles = makeStyles({
     minWidth: 700,
   },
 });
-let counter = 0;
-export default function CustomTable() {
+
+
+function CustomTable(props) {
   const classes = useStyles();
   const [state, setState] = useState([])
+  const [request, setRequest] = useState('')
   const [beginItem, setBeginItem] = useState(1);
   const [page, setPage] = React.useState(1);
   const handleChange = (event, value) => {
+    console.log(event, value)
     setPage(value);
   };
 
-    useEffect(() => {
-      console.log(beginItem)
-      getUser((page-1)*5+1, 5).then(
-        res => setState(res.data)
-      )
-    }, [page])
+  useEffect(async () => {
+    console.log(beginItem)
+    await getUser((page - 1) * 5 + 1, 5).then(
+      res => { setState(res.data) }
+    )
+    await getUser((page - 1) * 5 + 1, 5).then(
+      res => { setRequest(res.data) }
+    )
 
 
+  }, [page])
 
-    return (
-      <>
-        <TableContainer style={{width: "80%", margin: "auto" ,minHeight:"400px"}} component={Paper}>
-          <Table className={classes.table} aria-label="customized table">
-            <TableHead>
-              <TableRow>
-                <StyledTableCell align="right">نام کاربر</StyledTableCell>
-                <StyledTableCell align="right">مجموع مبالغ</StyledTableCell>
-                <StyledTableCell align="right">زمان ثبت سفارش</StyledTableCell>
-                <StyledTableCell align="right"></StyledTableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              <>
-                {state.map(fetchedData => console.log(fetchedData))}
-              </>
-              {state.map(data => (
-                <StyledTableRow  key={data.id}>
-              
-                  <StyledTableCell align="right">{data.id}</StyledTableCell>
-                      <StyledTableCell align="right">{data.name}</StyledTableCell>
-                      <StyledTableCell  align="right">{data.email}</StyledTableCell>
-                  <StyledTableCell  align="right">{'برسی سفارش'}</StyledTableCell>
-                  {/* <StyledTableCell style={{ border: '3px solid black' }} align="right">{''}</StyledTableCell> */}
-                </StyledTableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
-        <div style={{ width: "50%", margin: "auto", padding: '20px' }}>
-          {/* <button onClick={(e) => { setBeginItem(1); console.log(beginItem) }}>page1</button>
-          <button onClick={(e) => { setBeginItem(6); console.log(beginItem) }}>page2</button>
-          <button onClick={(e) => { setBeginItem(11); console.log(beginItem) }}>page3</button>
-          <button onClick={(e) => { setBeginItem(16); console.log(beginItem) }}>page4</button> */}
-          <div style={{direction:"ltr"}}>
-            {/* <Typography >Page: {page}</Typography> */}
-            <Pagination count={4} color="primary" page={page} onChange={handleChange} />
-          </div>
+  useEffect(() => {
+    console.log(request)
+    if (props.isFiltered) {
+      setState(request.filter(ele => ele.isReceived == props.isReceived))
+    }
+
+  }, [props])
+
+  return (
+    <>
+      <TableContainer style={{ width: "80%", margin: "auto", minHeight: "400px" }} component={Paper}>
+        <Table className={classes.table} aria-label="customized table">
+          <TableHead>
+            <TableRow>
+              <StyledTableCell align="right">نام کاربر</StyledTableCell>
+              <StyledTableCell align="right">مجموع مبالغ</StyledTableCell>
+              <StyledTableCell align="right">زمان ثبت سفارش</StyledTableCell>
+              <StyledTableCell align="right"></StyledTableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {console.log(props)}
+
+            {state.map(data => (
+              <StyledTableRow key={data.id}>
+                <StyledTableCell align="right">{data.id}</StyledTableCell>
+                <StyledTableCell align="right">{data.name}</StyledTableCell>
+                <StyledTableCell align="right">{(new Date(data.createdAt)).toLocaleDateString()}</StyledTableCell>
+                <StyledTableCell align="right">{'برسی سفارش'}</StyledTableCell>
+                {/* <StyledTableCell style={{ border: '3px solid black' }} align="right">{''}</StyledTableCell> */}
+              </StyledTableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <div style={{ width: "50%", margin: "auto", padding: '20px' }}>
+
+        <div style={{ direction: "ltr" }}>
+          <Pagination count={4} color="primary" page={page} onChange={handleChange} />
         </div>
-      </>
-    );
+      </div>
+    </>
+  );
+}
+
+const mapStateToProps = state => {
+  console.log(state)
+  return {
+    isFiltered: state.filterCustomTableReducer.isFiltered,
+    isReceived: state.filterCustomTableReducer.isReceived
   }
+}
+
+export default connect(mapStateToProps)(CustomTable);
--- shopping-project/src/pages/Manager/CustomPage.js
+++ shopping-project/src/pages/Manager/CustomPage.js
@@ -1,25 +1,40 @@
 import React, { Component } from "react";
 import { Button } from "@material-ui/core";
 import CustomTable from "../../layouts/Section/ManagerPage/CustomTable";
-import Header from '../../layouts/Header/Header'
+import ManagerHeader from '../../layouts/Header/ManagerHeader/ManagerHeader'
+import { connect } from 'react-redux';
+import { filterCustomTableAction } from "../../redux/Actions/filterCustomTableAction";
 
+class CustcomPage extends Component {
+    constructor(props) {
+        super(props)
+    }
 
-
-export class CustomPage extends Component {
-
+    state = {
+        isFiltered: false,
+        isReceived: false
+    }
+    componentDidUpdate() {
+      
+    }
     render() {
         return (
             <div >
-                <Header />
+                <ManagerHeader />
                 <div>
                     <h1>مدیریت سفارش ها</h1>
                     <span>سفارش های تحویل داده شده</span>
-                    <input type="radio" name="state" />
+                    <input type="radio" name="state" value='true'
+                        onChange={(e) => {
+                            this.setState({ isFiltered: true, isReceived: true })
+                            this.props.filterCustomTableAction(this.state)
+                        }} />
                     <span>سفارش های در انتظار ارسال</span>
-                    <input type="radio" name="state" />
-                    {/* <Button variant="contained" color="primary">
-          ذخیره
-        </Button> */}
+                    <input type="radio" name="state" value='false'
+                        onChange={(e) => {
+                            this.setState({ isFiltered: true, isReceived: false })
+                            this.props.filterCustomTableAction(this.state)
+                        }} />
                     <CustomTable />
 
                 </div>
@@ -27,3 +42,19 @@ export class CustomPage extends Component {
         );
     }
 }
+
+const mapStateToProps = state => {
+    return {
+        isFiltered: state.isFiltered,
+        isReceived: state.isReceived
+    }
+}
+const mapDispatchToProps = dispatch => {
+    console.log(dispatch)
+    return {
+        filterCustomTableAction: (flags) => { dispatch(filterCustomTableAction(flags)) },
+    }
+}
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(CustcomPage);
