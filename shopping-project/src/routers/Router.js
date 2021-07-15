641c0c5c4f6fbde7752a53873514d5160ef1233b
rasoul <rasoulkordestani12@gmail.com>
2021-07-15 17:21:47 +0430

    add LoginManagerPage to routes

--- shopping-project/src/routers/Router.js
+++ shopping-project/src/routers/Router.js
@@ -3,26 +3,18 @@ import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
 
 
 
-import { CustomPage } from '../pages/Manager/CustomPage';
+import  CustomPage  from '../pages/Manager/CustomPage';
 import { ProductPage } from '../pages/Manager/ProductPage';
-import { StockPricePage } from "../pages/Manager/StockPricePage"
-
+import { StockPricePage } from "../pages/Manager/StockPricePage";
+import { LoginManagerPage } from '../pages/Manager/LoginManagerPage';
 export default function Router() {
     return (
         < BrowserRouter>
             <div>
-               
-
-                <hr />
-
-                {/*
-            A <Switch> looks through all its children <Route>
-            elements and renders the first one whose path
-            matches the current URL. Use a <Switch> any time
-            you have multiple routes, but you want only one
-            of them to render at a time
-          */}
                 <Switch>
+                <Route exact path="/">
+                        <CustomPage />
+                    </Route>
                     <Route exact path="/CustomTable">
                         <CustomPage />
                     </Route>
@@ -31,6 +23,9 @@ export default function Router() {
                     </Route>
                     <Route path="/StockPriceTable">
                         <StockPricePage />
+                    </Route>
+                    <Route exact path="/LoginManagerPage">
+                       <LoginManagerPage />
                     </Route>
                 </Switch>
             </div>
