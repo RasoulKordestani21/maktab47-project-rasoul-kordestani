b9a3173c303519c3810442a682688b41e3da240f
rasoul <rasoulkordestani12@gmail.com>
2021-07-18 06:25:56 +0430

    deploy product modal and received modals (with maybe dummy tables!!!)

---
+++ shopping-project/src/components/Modals/ProductModal.js
@@ -0,0 +1,129 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Modal from "@material-ui/core/Modal";
+import CancelIcon from "@material-ui/icons/Cancel";
+import TextField from "@material-ui/core/TextField";
+import FormLabel from "@material-ui/core/FormLabel";
+import Button from "@material-ui/core/Button";
+import Input from "@material-ui/core/Input";
+import Select from "@material-ui/core/Select";
+import MenuItem from "@material-ui/core/MenuItem";
+import InputLabel from "@material-ui/core/InputLabel";
+import FormControl from "@material-ui/core/FormControl";
+import TextareaAutosize from "@material-ui/core/TextareaAutosize";
+import { connect } from 'react-redux';
+import { modalFlagAction } from '../../redux/Actions/modalFlagAction';
+import {useEffect} from 'react'
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: "absolute",
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    border: "2px solid #000",
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+    direction: "rtl",
+    height: "500px"
+  }
+}));
+
+ function ProductModal(props) {
+  const classes = useStyles();
+  // getModalStyle is not a pure function, we roll the style only on the first render
+  
+  const [open, setOpen] = React.useState(false);
+useEffect(() => {
+  setOpen(props.state.modalFlagReducer.productModalFlag)
+}, [props])
+  
+
+  const body = (
+    <div style={{
+      top:'50%',
+      left:'50%',
+      transform:`translate(-${50}%, -${50}%)`}} 
+      className={classes.paper}>
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between"
+        }}
+      >
+        <h2 id="simple-modal-title">افزودن/ویرایش کالا </h2>
+        <CancelIcon onClick={()=>{props.modalFlagAction(false)}}/>
+      </div>
+      <div>
+        <FormLabel style={{ display: "block" }}>تصویر کالا</FormLabel>
+        <Input type="file" variant="outlined" />
+      </div>
+      <div style={{ margin: "20px 0px" }}>
+        <FormLabel>نام کالا</FormLabel>
+        <TextField onFocus={() => {}} fullWidth />
+      </div>
+
+      <FormControl
+        style={{ width: "100%", marginTop: "20px", direction: "rtl" }}
+        variant="outlined"
+        className={classes.formControl}
+      >
+        <InputLabel id="demo-simple-select-outlined-label">
+          دسته بندی کالاها
+        </InputLabel>
+        <Select
+          labelId="demo-simple-select-outlined-label"
+          id="demo-simple-select-outlined"
+          // value={age}
+          // onChange={handleChange}
+          label="دسته بندی کالاها"
+        >
+          <MenuItem value="">
+            <em>نوع کالا را انتخاب کنید ...</em>
+          </MenuItem>
+          <MenuItem value={"dairy"}>لبنیات</MenuItem>
+          <MenuItem value={"health-Washing"}>مواد شوینده و بهداشتی</MenuItem>
+          <MenuItem value={"nuts"}>خشکبار</MenuItem>
+        </Select>
+      </FormControl>
+
+      <div style={{marginTop:"20px"}}>
+        <FormLabel style={{ display: "block" }}>توضیحات</FormLabel>
+        <TextareaAutosize style={{width:"100%",height:"80px"}} aria-label="empty textarea" placeholder="توضیحات کالا را وارد کنید" />
+      </div>
+    <div style={{display:'flex'}} >
+      <Button style={{backgroundColor:'purple',margin:'20px auto'}}>ذخیره</Button>
+      </div>
+    </div>
+
+  );
+
+  return (
+    <div>
+      
+      <Modal
+        open={open}
+        onClose={()=>{props.modalFlagAction(false)}}
+        aria-labelledby="simple-modal-title"
+        aria-describedby="simple-modal-description"
+      >
+        {body}
+      </Modal>
+    </div>
+  );
+}
+
+
+const mapStateToProps = state => {
+  console.log(state.modalFlagReducer.productModalFlag)
+  return {
+     state
+  }
+}
+const mapDispatchToProps = dispatch => {
+  console.log(dispatch)
+  return {
+      modalFlagAction: (flag) => { dispatch(modalFlagAction(flag)) }
+  }
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(ProductModal)
---
+++ shopping-project/src/components/Modals/customModals/ReceivedCustomModal.js
@@ -0,0 +1,122 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Modal from "@material-ui/core/Modal";
+import CancelIcon from "@material-ui/icons/Cancel";
+import TextField from "@material-ui/core/TextField";
+import FormLabel from "@material-ui/core/FormLabel";
+import Button from "@material-ui/core/Button";
+import Input from "@material-ui/core/Input";
+import Select from "@material-ui/core/Select";
+import MenuItem from "@material-ui/core/MenuItem";
+import InputLabel from "@material-ui/core/InputLabel";
+import FormControl from "@material-ui/core/FormControl";
+import TextareaAutosize from "@material-ui/core/TextareaAutosize";
+import { connect } from 'react-redux';
+import { modalFlagAction } from '../../../redux/Actions/modalFlagAction';
+import { useEffect } from 'react';
+import DummyTable from "../../Modals/DummyTable/DummyTable";
+
+const useStyles = makeStyles((theme) => ({
+    paper: {
+        position: "absolute",
+        width: 400,
+        backgroundColor: theme.palette.background.paper,
+        border: "2px solid #000",
+        boxShadow: theme.shadows[5],
+        padding: theme.spacing(2, 4, 3),
+        direction: "rtl",
+        height: "500px"
+    }
+}));
+
+function ReceivedCustomModal(props) {
+    const classes = useStyles();
+    // getModalStyle is not a pure function, we roll the style only on the first render
+
+    const [open, setOpen] = React.useState(false);
+    useEffect(() => {
+        setOpen()
+    }, [props])
+
+
+    const body = (
+        <div style={{
+            top: '50%',
+            left: '50%',
+            transform: `translate(-${50}%, -${50}%)`
+        }}
+            className={classes.paper}>
+            <div
+                style={{
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "space-between"
+                }}
+            >
+                <h2 id="simple-modal-title">افزودن/ویرایش کالا </h2>
+                <CancelIcon onClick={() => { props.modalFlagAction(false) }} />
+            </div>
+
+
+            <div>
+                <div>
+                    <span>نام مشتری :</span><span>علی کاظمی</span>
+                </div>
+                <div>
+                    <span>آدرس: </span><span>تهران- خ انقلاب-کوچه ی سوم- پلاک ۳</span>
+                </div>
+                <div>
+                    <span>تلفن : </span><span>۰۹۱۳۸۸۲۱۹۹۸</span>
+                </div>
+                <div>
+                    <span>زمان تحویل : </span><span>۱۴۰۰/۰۳/۱۲</span>
+                </div>
+                <div>
+                    <span>زمان سفارش :</span><span>۱۴۰۰/۰۳/۱۲</span>
+                </div>
+            </div>
+            <div style={{ backgroundColor: 'yellow', width: '100%', height: '40px' }}>
+                <DummyTable />
+            </div>
+            <div style={{ display: 'flex' }} >
+                <Button style={{ backgroundColor: 'purple', margin: '220px auto' }}>ذخیره</Button>
+            </div>
+
+
+            <div style={{ display: 'flex' }} >
+                <Button style={{ backgroundColor: 'purple', margin: '20px auto' }}>ذخیره</Button>
+            </div>
+        </div>
+
+    );
+
+    return (
+        <div>
+
+            <Modal
+                open={open}
+                onClose={() => { props.modalFlagAction(false) }}
+                aria-labelledby="simple-modal-title"
+                aria-describedby="simple-modal-description"
+            >
+                {body}
+            </Modal>
+        </div>
+    );
+}
+
+
+const mapStateToProps = state => {
+    console.log(state.modalFlagReducer.ReceivedCustomModalFlag)
+    return {
+        state
+    }
+}
+const mapDispatchToProps = dispatch => {
+    console.log(dispatch)
+    return {
+        modalFlagAction: (flag) => { dispatch(modalFlagAction(flag)) }
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ReceivedCustomModal)
