ba3c8fc8898a76cee024cbca672a8df4d32f294e
rasoul <rasoulkordestani12@gmail.com>
2021-07-31 00:19:22 +0430

    use sass styles and new logics in product card

--- shopping-project/src/components/ProductCard/ProductCard.js
+++ shopping-project/src/components/ProductCard/ProductCard.js
@@ -1,25 +1,34 @@
 import React, { Component } from 'react'
+import styles from '../../assets/style/style.module.css';
 export default class ProductCard extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            products:''
+            products: ''
         }
     }
-    componentDidUpdate(){
+    componentDidUpdate() {
     }
-   
+
     render() {
-       return (
-           
-            <div style={{ display: "flex", backgroundColor: 'yellow', padding: "10px" }}>
+        return (
+
+            <div
+
+                onClick={(e) => this.props.click(this.props.id, this.props.group)}
+                className={styles['main-section-product-card']}
+
+            >
                 <img
-              src={this.props.product && this.props.product.image}
-                    width="100px" height="100px"
+                    src={this.props.product && this.props.product.image}
+                    width="150px" height="150px"
                 />
-                <div style={{ backgroundColor: 'teal', width: '50%' }}>
-                    <div>{this.props.product && this.props.product.name}</div>
-                    <div>{this.props.product && this.props.product.price}تومان</div>
+                <div
+                    className={styles['main-section-product-card-text']}
+                    // style={{ margin: '4px 20px', display: 'flex', flexDirection: 'column', justifyContent: 'space-around' }}
+                >
+                    <div >{this.props.product && this.props.product.name}</div>
+                    <div >{this.props.product && this.props.product.price}تومان</div>
                 </div>
             </div>
         )
