97baf35e6b7dcf8335b154c5e91b82415fee552b
rasoul <rasoulkordestani12@gmail.com>
2021-07-31 00:24:07 +0430

    some unusable changes

--- shopping-project/src/pages/App.js
+++ shopping-project/src/pages/App.js
@@ -1,18 +1,16 @@
 import React, { Component } from "react";
 import { Header, Section } from "../layouts/terminal";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-import  CustomPage  from '../pages/Manager/CustomPage';
-import  ProductPage  from '../pages/Manager/ProductPage';
+import CustomPage from '../pages/Manager/CustomPage';
+import ProductPage from '../pages/Manager/ProductPage';
 import { StockPricePage } from '../pages/Manager/StockPricePage';
-
 import store from "../redux/store";
 import { Provider } from "react-redux";
-
 import Router from "../routers/Router";
-import '../assets/style/style.css'
 // import { typography } from "@material-ui/system";
 import { createMuiTheme } from "@material-ui/core";
 import { ThemeProvider } from "@material-ui/styles";
+// import '../assets/style/style.module.css';
 
 const theme = createMuiTheme({
   typography: {
@@ -35,9 +33,9 @@ export class App extends Component {
   render() {
     return (
 
-      <Provider store={store}>
-        <ThemeProvider theme={theme}>
-          <Router />
+      <Provider store={store} >
+        <ThemeProvider theme={theme} injectFirst>
+          <Router className='dummy' />
         </ThemeProvider>
       </Provider>
     );
