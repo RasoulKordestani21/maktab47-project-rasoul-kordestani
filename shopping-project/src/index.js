73aeb5a98efceab521e42c8ab9fa6a14d8e908bf
rasoul <rasoulkordestani12@gmail.com>
2021-07-30 23:26:40 +0430

    provide index page for  using jss-rtl

--- shopping-project/src/index.js
+++ shopping-project/src/index.js
@@ -2,7 +2,23 @@ import React from "react";
 import { App } from "./pages/terminal";
 import ReactDOM from "react-dom";
 import reportWebVitals from "./reportWebVitals";
-ReactDOM.render(<App />, document.getElementById("root"));
+import { create } from 'jss';
+import rtl from 'jss-rtl';
+import { StylesProvider, jssPreset, ThemeProvider } from '@material-ui/core/styles';
+import RtlTheme from "./assets/style/materialUI_styles/rtlTheme";
+import './assets/fonts/font.css'
+// import './assets/style/style.module.scss'
+import styles from './assets/style/style.module.css'
+import { style } from "@material-ui/system";
+// Configure JSS
+const jss = create({ plugins: [...jssPreset().plugins, rtl()] });
+
+
+
+
+ReactDOM.render(
+ <App />
+    , document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
